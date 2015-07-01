93e3a5c5d788f523d4096ab6ae01fb5e37530937
Auston <auston.salvana@live.com>
2015-06-30 23:00:10 -0700

    saving

--- Adam/main.c
+++ Adam/main.c
@@ -7,9 +7,35 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    char response[100];
+    
+    printf("what's good\n");
+    
+    fgets(response, 100, stdin);
+    
+    if (strcmp(response, "Adam\n") == 0) {
+        
+        printf("Fine. Be that way!");
+        
+    }
+    else if (strncmp(response, "!\n", 100) == 0){
+        printf("Woah, chill out!");
+    }
+    else if (strncmp(response, "?\n", 100) == 0){
+        printf("Sure.");
+    }
+    else if (strcmp(response, "exit\n") == 0){
+        return 0;
+    }
+    else{
+    
+        printf("Whatever.");
+    }
+    
     return 0;
 }
